0fbd26d83abeefdd873173c9b5ee8d93ecac4467
tvestal <tvestal@live.unc.edu>
2021-11-08 12:37:50 -0500

    maxAndMin bugfix

--- src/mild/mild_1.js
+++ src/mild/mild_1.js
@@ -44,7 +44,7 @@ export function getIncreasingArray(startNumber, endNumber) {
  */
 export function maxAndMin(numbers) {
 
-    return {"min": Math.min.apply(this, numbers), "max": Math.max.apply(this, numbers)};
+    return {"min": Math.min(...numbers), "max": Math.max(...numbers)};
 
 }
 
